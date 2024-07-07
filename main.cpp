69449b38f990e3ee7acc07139c62a11f0844d48c
Alice (BrezinskiyVN) <alice@example.com>
2024-07-07 14:29:51 +0300

    int заменен на double

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
     cout << "Enter A and B: \n";
-    int a, b;
+    double a, b;
     cin >> a >> b;
     cout << a << ' ' << b;
     cout << "A + B = " << a + b << '\n';
